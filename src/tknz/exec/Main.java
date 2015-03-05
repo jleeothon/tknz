327590038f93de34f89c1a0d6e5accb362795681
Johnny Lee <jleeothon@gmail.com>
2015-03-04 19:38:27 -0600

    Simplified approach to the main executable
    Instead of receiving several grammar files, we will receive a single
    one.

--- src/tknz/exec/Main.java
+++ src/tknz/exec/Main.java
@@ -7,23 +7,18 @@ import java.io.InputStreamReader;
 
 import org.antlr.v4.runtime.*; 
 
-import tknz.parser.TknzEngine;
+import tknz.parser.Engine;
 
 public class Main {
 	public static void main(String[] args) {
-		ANTLRInputStream[] grammars = null;
+		ANTLRInputStream grammar = null;
 		InputStreamReader input = null;
 		
 		try {
-			input = new InputStreamReader(new FileInputStream(args[0]));
-			grammars = new ANTLRInputStream[args.length - 1];
-			for (int i = 1; i < args.length; i++) {
-				grammars[i - 1] = new ANTLRInputStream(new FileInputStream(args[i]));
-			}
+			grammar = new ANTLRInputStream(new FileInputStream(args[0]));
+			input = new InputStreamReader(new FileInputStream(args[1]));
 		} catch (IndexOutOfBoundsException e) {
-			String[] usage = {
-					"Usage: tknz <input> [<grammar1.tkn>, ...]"
-					};
+			String[] usage = {"Usage: tknz <grammar> <input>"};
 			for (String u : usage) {
 				System.out.println(u);
 			}
@@ -36,10 +31,7 @@ public class Main {
 			System.exit(1);
 		}
 		
-		TknzEngine engine = new TknzEngine(input);
-		for (ANTLRInputStream stream : grammars) {
-			engine.load(stream);
-		}
-		
+		Engine engine = new Engine(input);
+		engine.load(grammar);
 	}
 }
